ab9bb20298dcb05938cbc1ed9c4a80834f8711ec
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-17 01:20:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,14 +22,9 @@ end
 
 def monopoly_with_third_tier
 	monopoly = {
-	 
-	 
-	 
-	 
-	 
-	 # :railroads => {pieces:4, names: {} rent_in_dollars:{},
-	    one_piece_own:25}
-	}}
+	 :railroads => {pieces:4, names: {} rent_in_dollars:{},
+	    one_piece_owned:25}
+	}
 
 end
 
